d9d1baebfa12abb6ce7d79da11ce892a97dface9
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-09-01 11:14:52 +0300

    Using CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import About from "./pages/About";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <div>
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/product" element={<Product />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      App Layout
+      <AppNav />
+    </div>
+  );
+}
+
+export default AppLayout;
