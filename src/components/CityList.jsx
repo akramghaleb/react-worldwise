2960b348e7d81e4317bc999095732898c27455f3
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-09-03 10:16:07 +0300

    Implementing the Countries List

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -12,7 +12,7 @@ function CityList({ cities, isLoading }) {
     );
   }
   return (
-    <ul style={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) => (
         <CityItem city={city} key={city.id} />
       ))}
