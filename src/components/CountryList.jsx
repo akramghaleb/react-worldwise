724845723df36887d3f10c5004349045863adfad
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-09-08 13:17:26 +0300

    Consuming the CitiesContext

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -3,8 +3,10 @@ import Spinner from "./Spinner";
 import Message from "./Message";
 
 import styles from "./CountryList.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) {
     return <Spinner />;
   }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-const { createContext, useState, useEffect } = require("react");
+import { createContext, useState, useEffect, useContext } from "react";
 
 const CitiesContext = createContext();
 
@@ -32,4 +32,11 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
