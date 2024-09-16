28efacac7db1da54d3fb9bb92b37542b5fbecc36
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-09-16 13:46:18 +0300

    Deleting a City

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -53,7 +53,21 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch (err) {
-      alert("There was an error loading data...");
+      alert("There was an error creating city data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (err) {
+      alert("There was an error deleting city data...");
     } finally {
       setIsLoading(false);
     }
@@ -61,7 +75,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
